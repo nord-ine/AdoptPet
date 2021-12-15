bcb07a420baf46a5d04880f7f4b1a17d5e281865
Noureddine Khelifaoui <nkhelifaoui@pocketresult.com>
2021-12-15 14:00:52 +0100

    first commit

---
+++ src/main/Person.java
@@ -0,0 +1,26 @@
+package main;
+
+/**
+ * Décrivez votre classe Human ici.
+ *
+ * @author (votre nom)
+ * @version (un numéro de version ou une date)
+ */
+public class Person
+{
+    // variables d'instance - remplacez l'exemple qui suit par le vôtre
+
+    /**
+     * Constructeur d'objets de classe Human
+     */
+    public Person()
+    {
+        // initialisation des variables d'instance
+        
+    }
+
+    
+    public void giveNameToPet(Pet p,String name){
+        p.setName(name);
+    }
+}
---
+++ src/main/Pet.java
@@ -0,0 +1,44 @@
+package main;
+
+/**
+ * Décrivez votre classe Pet ici.
+ *
+ * @author (votre nom)
+ * @version (un numéro de version ou une date)
+ */
+public class Pet
+{
+    private int age = 1;
+    private String color = "";
+    private String name = "";
+    /**
+     * Constructeur d'objets de classe Pet
+     */
+    public Pet()
+    {
+        // initialisation des variables d'instance
+        
+    }
+    
+    public int getAge(){
+        return this.age;
+    }
+    public String getColor(){
+        return this.color;
+    }
+    
+    public void setAge(int age){
+        this.age = age;
+    }
+    public void setColor(String color){
+        this.color = color;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return this.name;
+    }
+     
+}
